09c044ad38fcdc12a3e5eb43813849cdf3a5f28d
Kourosh <Kourosh.ilchi@gmail.com>
2026-07-05 18:56:19 +0330

    Add imports for additional model architectures in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,7 @@
 from vision.resnet import *
+from vision.AlexNet import *
+from vision.EfficientNet import *
+from vision.GoogleNet import *
+from vision.MobileNet import *
+from vision.VGGNet import *
+from vision.DenseNet import * 
