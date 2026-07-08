652675c07a187b3967f21c3ff4da5362bb742f9f
Kourosh <Kourosh.ilchi@gmail.com>
2026-07-08 11:32:43 +0330

    Implement DenseNet architecture with dynamic DenseBlock and TransitionBlock integration

--- models/vision/DenseNet/DenseNet.py
+++ models/vision/DenseNet/DenseNet.py
@@ -37,3 +37,55 @@ class TransitionBlock(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
+    
+class DenseNet(nn.Module):
+    def __init__(self, 
+                 in_channels=3, 
+                 num_classes=1000, 
+                 growth_rate=32,      
+                 theta=0.5,
+                 architecture=(6, 12, 24, 16)): 
+        
+        super().__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        
+        self.blocks = nn.ModuleList()
+        in_channels = 64
+        
+        for i, num_layers in enumerate(architecture):
+            self.blocks.append(DenseBlock(num_layers, in_channels, growth_rate))
+            
+            in_channels = in_channels + num_layers * growth_rate
+            
+            if i != len(architecture) - 1:
+                self.blocks.append(TransitionBlock(in_channels, theta))
+                in_channels = int(in_channels * theta)
+        
+        self.classifier = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(in_channels, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        for block in self.blocks:
+            x = block(x)
+        x = self.classifier(x)
+        return x
+
+if __name__=="__main__":
+    model = DenseNet(3, 100).to('cuda')
+    picture = torch.randn([1, 3, 224, 224]).to('cuda')
+    with torch.no_grad():
+        outputs = model(picture)
+    summary(model, (3, 224, 224))
+    print(outputs.shape)
